a717e2a80dba967aaa329919b4ee73c524852cdc
GreenPines <sgreenpine@gmail.com>
2017-07-06 16:21:32 +0800

    Initial Commit

---
+++ SS9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SS9
+//
+//  Created by 沈松青 on 2017/7/6.
+//  Copyright © 2017年 沈松青. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
