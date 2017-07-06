3683d1922f335989efda8d2731465c4d71e63ae9
GreenPines <sgreenpine@gmail.com>
2017-07-06 09:52:21 +0800

    Add a search bar

---
+++ SS9/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  SS9
+//
+//  Created by 沈松青 on 2017/7/6.
+//  Copyright © 2017年 沈松青. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
